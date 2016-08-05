30600a6eba873ea209110466b256ea662fdf4cca
Cipher-geek-Studios <svdataworks@outlook.com>
2016-08-05 17:08:15 +0530

    Added delete to single item
    Select the checkbox and delete the items checked: Added.
    Aslo added jQuery & Readme.md

--- db.js
+++ db.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var sql = require('sql.js');
 var bfr = fs.readFileSync('./tmp/test.db');
 var db = new sql.Database(bfr);
+window.$ = window.jQuery = require('jquery');
 var text
 var clear = "";
 
@@ -17,7 +18,7 @@ function shwtb() {
         for (i=0; i<lenth; i++){
             var dbrow = dts[0].values[i];
             var spltarr = String(dbrow).split(',');
-            text += "<li class='list-group-item'><input type='checkbox' value = '"+ spltarr[0] +"'/>" + spltarr[1] + "</li>";
+            text += "<li class='list-group-item'><input id='checklist' type='checkbox' value = '"+ spltarr[0] +"'/>" + spltarr[1] + "</li>";
         }
         text += "</ul>";
         document.getElementById("list").innerHTML = text;
@@ -51,4 +52,21 @@ function deltb() {
     fs.writeFileSync('./tmp/test.db', buffer);
     alert("All Cleared")
     document.getElementById("list").innerHTML = clear;
+}
+
+function delitem() {
+    var checkedValues = $('input[id="checklist"]:checked').map(function() {
+        return this.value;
+    }).get();
+    var checvaspl = String(checkedValues).split(',');
+    var conf = confirm("Are you sure wanted to delete "+checvaspl.length+" items from the tabel");
+    if (conf){
+        for (j=0; j<checvaspl.length; j++){
+            db.exec("DELETE FROM data WHERE id='"+ checvaspl[j] +"'");
+            var data = db.export();
+            var buffer = new Buffer(data);
+            fs.writeFileSync('./tmp/test.db', buffer);
+        }
+    shwtb();
+    }
 }
---
+++ readme.md
@@ -0,0 +1,10 @@
+Hello coders this is Roshan from Cipher geek Studios.
+
+The above repository is an example to connect electron to local db and use all the db function.
+
+You can INSERT, SELECT, DELETE data rows from the electron using javascript.
+My db structure is
+path: ./tmp/test.db
+datasource: sql.js (sqlite3)
+TABLE: data(id INTEGER PRIMARY KEY AUTOINCREMENT,
+        state CHAR[50]);
