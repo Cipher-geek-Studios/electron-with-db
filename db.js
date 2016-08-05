5b0d197b3d336e264c9d7a8e37258c63eab73ae8
Cipher-geek-Studios <svdataworks@outlook.com>
2016-08-05 15:31:36 +0530

    electron-with-db
    Created a electron app (electron must be installed globally with npm
    'npm install electron -g'

---
+++ db.js
@@ -0,0 +1,52 @@
+var fs = require('fs');
+var sql = require('sql.js');
+var bfr = fs.readFileSync('./tmp/test.db');
+var db = new sql.Database(bfr);
+var text
+var clear = "";
+
+function showts() {
+    shwtb();
+}
+
+function shwtb() {
+    var dts = db.exec('SELECT * FROM data');
+    if (dts.length > 0) {
+        var lenth = dts[0].values.length;
+        text = "<ul class='list-group'>";
+        for (i=0; i<lenth; i++){
+            text += "<li class='list-group-item'><input type='checkbox'/>" + dts[0].values[i] + "</li>";
+        }
+        text += "</ul>";
+        document.getElementById("list").innerHTML = text;
+    }
+    else {
+        alert("No data to display")
+    }
+}
+
+function insdb() {
+    var txtb = document.getElementById('textss').value;
+    db.run("INSERT INTO data (state) VALUES ('" + txtb +"')");
+    var data = db.export();
+    var buffer = new Buffer(data);
+    fs.writeFileSync('./tmp/test.db', buffer);
+    shwtb();
+}
+// Create the table with command.
+// function cretb() {
+//     db.exec('CREATE TABLE data(state char[50]);');
+//     var data = db.export();
+//     var buffer = new Buffer(data);
+//     fs.writeFileSync('./tmp/test.db', buffer);
+//     alert("Table Created")
+// }
+
+function deltb() {
+    db.exec('DELETE FROM data',[]);
+    var data = db.export();
+    var buffer = new Buffer(data);
+    fs.writeFileSync('./tmp/test.db', buffer);
+    alert("All Cleared")
+    document.getElementById("list").innerHTML = clear;
+}
